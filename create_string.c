d6f845ddcc0f716f932c8e51a9e6c5f48983b70b
Ripon <ripon365cse@gmail.com>
2025-04-21 15:36:36 +0600

    create string

---
+++ create_string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    char name[]="Ripon";
+    for(int i=0;i<5;i++){
+        printf("%c\n",name[i]);
+    }
+    return 0;
+
+}
--- read_2d_array.c
+++ read_2d_array.c
@@ -2,14 +2,17 @@
 
 int main(){
 
-    int matrix [2][3]={
+    int matrix [3][3]={
         {8,5,2},
-        {7,9,0}
+        {7,9,0},
+        {2,3,4}
         };
 
         int i,j;
-        for(i=0;i<2;i++){
+        for(i=0;i<3;i++){
+            printf("Row no :%d\n",i);
             for(j=0;j<3;j++){
+                printf("Column no :%d\n",j);
                 printf("%d\n",matrix[i][j]);
             }
         }
