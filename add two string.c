eb0c54743ecd5a8410f7fb816317f9e40f347bb7
Ripon <ripon365cse@gmail.com>
2025-04-22 00:28:41 +0600

    add to two string

---
+++ add two string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char welcome[]="Welcome to Raihan Ali Ripon ";
+    char details[]="Hello world";
+    strcat(welcome,details);
+    printf("%s\n",welcome);
+
+    return 0;
+
+}
