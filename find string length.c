66d69ab4d0a1fd807aa2e1cc0051e3966378cc00
Ripon <ripon365cse@gmail.com>
2025-04-22 00:21:43 +0600

    string length

---
+++ find string length.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char welcome[]="Welcome to Raihan Ali Ripon";
+    //char name[]="Raihan";
+    printf("%d\n",strlen(welcome));
+   // printf("%d",sizeof(welcome));
+    //printf("%s",name);
+
+    return 0;
+
+}
---
+++ string_example.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    char welcome[]="Wel\"come\" to'\0'Raihan";
+    char name[]="Raihan";
+    printf("%s",welcome);
+    printf("%s",name);
+
+    return 0;
+
+}
