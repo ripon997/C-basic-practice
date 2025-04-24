4e2a6280492f1b95283f0a96fa827d3076b03067
Ripon <ripon365cse@gmail.com>
2025-04-24 19:24:36 +0600

    add parameter in function

---
+++ function_example01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+
+    void calculate_sum(){
+    int a=12,b=45,sum;
+    sum=a+b;
+    printf("in function sum is : %d",sum);
+    }
+
+int main(){
+
+    calculate_sum();
+
+
+    return 0;
+}
---
+++ function_parameter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+    void myFunction(char name[]){
+    printf("%s\n",name);
+
+    }
+
+int main(){
+    myFunction("Raihan");
+    myFunction("Ali");
+    myFunction("Ripon");
+
+    return 0;
+}
