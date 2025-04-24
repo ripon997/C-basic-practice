84b0450d164a7b0a10812bccc4ae0ff3a43df743
Ripon <ripon365cse@gmail.com>
2025-04-24 17:42:30 +0600

    create function

---
+++ create_function.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+
+    void myFunction(){
+    printf("My first function\n");
+    }
+
+int main(){
+
+    myFunction();
+    myFunction();
+    myFunction();
+    myFunction();
+
+
+    return 0;
+}
--- pointer and array.c
+++ pointer and array.c
@@ -4,7 +4,8 @@ int main(){
 
     int numbers[7] = {10,23,23,44,55,66,77};
     int *ptr = numbers;
-
+    *(numbers)=15;
+    *(numbers+1)=20;
     for (int i=0;i<7;i++){
         printf("%d\n",*(ptr+i));
     }
