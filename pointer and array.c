3dbda176158def70a3ce5a9e39fc82efee3584bd
Ripon <ripon365cse@gmail.com>
2025-04-24 17:16:44 +0600

    pointer and array

---
+++ pointer and array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    int numbers[7] = {10,23,23,44,55,66,77};
+    int *ptr = numbers;
+
+    for (int i=0;i<7;i++){
+        printf("%d\n",*(ptr+i));
+    }
+   /* for(int index=0;index<7;index++){
+        printf("%p\n",&numbers[index]);
+    }*/
+    //printf("%d\n",*numbers);
+    //printf("%p\n",&numbers);
+
+    return 0;
+}
