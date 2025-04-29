fd654a16f9eade1207fc1dc3756279b8fa04da24
Ripon <ripon365cse@gmail.com>
2025-04-29 18:12:32 +0600

    math header

---
+++ math_header.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+    float a=16;
+    float b=1.1;
+    float c= 1.9;
+    float d;
+    printf("%.1f\n",sqrt(a));
+    printf("%.1f\n",ceil(b));
+    printf("%.1f\n",floor(c));
+    printf("%f",pow(4,3));
+
+    return 0;
+}
+
+
