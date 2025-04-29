5b65a50bef3e18efbceb4ceb974be33dd376abd5
Ripon <ripon365cse@gmail.com>
2025-04-30 01:35:48 +0600

    copy structure element

---
+++ structure_element_equal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+struct myStructure{
+    int myNum;
+    char myLetter;
+    char myString[30];
+};
+
+int main(){
+    struct myStructure s1 = {15,'R',"Ripon"};
+    struct myStructure s2;
+    s2=s1;
+
+
+
+    //printf("%d %c %s\n",s1.myNum,s1.myLetter,s1.myString);
+    printf("%d %c %s\n",s2.myNum,s2.myLetter,s2.myString);
+
+}
+
+
