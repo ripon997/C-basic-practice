e2ba4cdfe09201d270c04b00254dec7ec9195295
Ripon <ripon365cse@gmail.com>
2025-04-30 22:00:59 +0600

    memory size

---
+++ memory_size01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    int number ;
+    char c;
+    double f;
+    int students[20];
+
+    printf("%lu\n",sizeof(number));
+    printf("%lu\n",sizeof(c));
+    printf("%lu\n",sizeof(f));
+    printf("%lu\n",sizeof(students));
+    return 0;
+}
